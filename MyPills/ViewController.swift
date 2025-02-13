ef4946edd59490282c9e9823ba8753601cc841b9
KatSchreiner <kat.schreiner@yandex.ru>
2025-02-13 18:20:33 +0300

    Initial Commit

---
+++ MyPills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPills
+//
+//  Created by Екатерина Шрайнер on 13.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPillsTests/MyPillsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPillsTests.swift
+//  MyPillsTests
+//
+//  Created by Екатерина Шрайнер on 13.02.2025.
+//
+
+import Testing
+@testable import MyPills
+
+struct MyPillsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
