790cfe2d39532ed90e003c8f626bbb10bbe1a76a
KatSchreiner <kat.schreiner@yandex.ru>
2025-02-14 17:48:43 +0300

    Добавила кнопку addPillButton

---
+++ MyPills/MyPillsView/MyPillsViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  MyPills
+//
+//  Created by Екатерина Шрайнер on 13.02.2025.
+//
+
+import UIKit
+
+class MyPillsViewController: UIViewController {
+    // MARK: - Public Properties
+    lazy var addPillButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(systemName: "plus")
+        button.setImage(image, for: .normal)
+        button.backgroundColor = .lBlue
+        button.tintColor = .white
+        button.layer.cornerRadius = 40
+        button.addTarget(self, action: #selector(didTapAddPillButton), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Private Properties
+    
+    // MARK: - Initializers
+    
+    // MARK: - View Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    // MARK: - Overrides Methods
+    
+    // MARK: - Navigation
+    
+    // MARK: - IB Actions
+    @objc
+    private func didTapAddPillButton() {
+        
+    }
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        
+        [addPillButton].forEach { view in
+            self.view.addSubview(view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraint()
+    }
+    
+    private func addConstraint() {
+        NSLayoutConstraint.activate([
+            addPillButton.widthAnchor.constraint(equalToConstant: 80),
+            addPillButton.heightAnchor.constraint(equalToConstant: 80),
+            addPillButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            addPillButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            addPillButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30)
+        ])
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    // MARK: – UITableViewDelegate
+    
+    // MARK: - UICollectionViewDataSource
+    
+    // MARK: – UICollectionViewDelegate
+
+}
+
